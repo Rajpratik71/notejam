0a2f4f8ac99fd0d3ad62e97d891ace050ccc74af
TheRenegadeOfFunk <therenegadeoffunk@gmail.com>
2018-03-01 19:54:51 +0900

    Schema update and setup.sh script improvements
    Ran into a few errors after doing more testing on
    the app. Turns out that AUTO_INCREMENT needed to
    be set on all three tables.
    
    Also, trying to get setup.sh to do more work. I
    want the user to be able to run that and have it
    deal with the docker image build and push to the
    Amazon container repo. Eventually I may make this
    script handle the terraform tasks too.

--- flask/conf/setup.sh
+++ flask/conf/setup.sh
@@ -8,4 +8,28 @@
 
 # Run terraform init
 
-# DB Endpoint URL: terraform-20180226041933478800000001.cbcuqxe20rfg.ap-northeast-1.rds.amazonaws.com
+# Build base docker image
+
+docker build -t notejam_base
+
+IMAGE_ID=`docker images | grep -i notejam_base | awk '{print $3}'`
+
+# There was some weirdness with string quotes in the output.
+
+# TODO: Make this cleaner...
+
+URI_STRING=`aws ecr describe-repositories | grep -i repositoryUri | awk '{print $2}' | sed -e 's/^"//' -e 's/"$//'`
+
+ECR_URI=`echo $URI_STRING | rev | cut -c 3- | rev`
+
+docker tag $IMAGE_ID $ECR_URI2
+
+# This command might fail if a lot of time has passed and you are logged out of ecr.
+
+# If it fails it should tell you what command to run to log back in. Do it and re-run the script.
+
+docker push $ECR_URI
+
+# Instruct user to set up env variables. Terraform will need several parameters to do its magic.
+
+
