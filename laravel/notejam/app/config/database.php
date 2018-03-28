be5e6ae47042dcc1408d4d0758b72ac384fca49f
John Ginsberg <john.ginsberg@gmail.com>
2018-03-28 16:54:48 +0100

    Additional DB and Laravel setup

--- laravel/notejam/app/config/database.php
+++ laravel/notejam/app/config/database.php
@@ -26,7 +26,7 @@ return array(
 	|
 	*/
 
-	'default' => 'sqlite',
+	'default' => 'mysql',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'database',
-			'username'  => 'root',
-			'password'  => '',
+                        'host'      => $_SERVER['RDS_HOSTNAME'],
+                        'database'  => $_SERVER['RDS_DB_NAME'],
+                        'username'  => $_SERVER['RDS_USERNAME'],
+                        'password'  => $_SERVER['RDS_PASSWORD'],
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
