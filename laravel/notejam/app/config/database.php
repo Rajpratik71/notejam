8d4816c00e8dc08b9c786d7a169b91d24cfd09ad
John Ginsberg <john.ginsberg@gmail.com>
2018-03-28 16:55:59 +0100

    Tabs. Not spaces.

--- laravel/notejam/app/config/database.php
+++ laravel/notejam/app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-                        'host'      => $_SERVER['RDS_HOSTNAME'],
-                        'database'  => $_SERVER['RDS_DB_NAME'],
-                        'username'  => $_SERVER['RDS_USERNAME'],
-                        'password'  => $_SERVER['RDS_PASSWORD'],
+			'host'      => $_SERVER['RDS_HOSTNAME'],
+			'database'  => $_SERVER['RDS_DB_NAME'],
+			'username'  => $_SERVER['RDS_USERNAME'],
+			'password'  => $_SERVER['RDS_PASSWORD'],
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
