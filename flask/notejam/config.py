57dce3959b80e62c55debdd03e64e4b7ad55d139
Felipe Rodrigues Maia <frmaia.br@gmail.com>
2018-04-18 16:12:13 -0300

    Circle-CI deployment + changing DB connection to MySQL

--- flask/notejam/config.py
+++ flask/notejam/config.py
@@ -7,7 +7,7 @@ class Config(object):
     SECRET_KEY = 'notejam-flask-secret-key'
     CSRF_ENABLED = True
     CSRF_SESSION_KEY = 'notejam-flask-secret-key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'notejam.db')
+    SQLALCHEMY_DATABASE_URI = '__SQLALCHEMY_DATABASE_URI_CI_CHANGE_VAR__'
 
 
 class ProductionConfig(Config):
@@ -17,7 +17,9 @@ class ProductionConfig(Config):
 class DevelopmentConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'notejam.db')
 
 
 class TestingConfig(Config):
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'notejam.db')
