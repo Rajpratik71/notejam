d3361a5c1021c64e9c40a3fa533dc8dec4164724
Felipe Rodrigues Maia <frmaia.br@gmail.com>
2018-04-20 15:48:36 -0300

    Circle-CI + AWS CodeDeploy

---
+++ aws_codedeploy/install_dependencies.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+echo '-- Installing application/server dependencies --'
+sudo /usr/local/bin/pip install -r /opt/app/flask/requirements.txt
--- flask/notejam/__init__.py
+++ flask/notejam/__init__.py
@@ -3,10 +3,16 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
 from flask.ext.mail import Mail
 
+import logging
+
 # @TODO use application factory approach
 app = Flask(__name__)
 app.config.from_object('notejam.config.Config')
 db = SQLAlchemy(app)
+
+gunicorn_logger = logging.getLogger('gunicorn.error')
+app.logger.handlers = gunicorn_logger.handlers
+app.logger.setLevel(gunicorn_logger.level)
 
 login_manager = LoginManager()
 login_manager.login_view = "signin"
