7e5e40fa5ea83f1bbdd604157d4b65381ab15813
Tomas Fedor <tomas.fedor@kiwi.com>
2019-01-13 22:35:26 +0100

    Indentation.

--- flask/notejam/config.py
+++ flask/notejam/config.py
@@ -7,7 +7,7 @@ class Config(object):
     SECRET_KEY = 'notejam-flask-secret-key'
     CSRF_ENABLED = True
     CSRF_SESSION_KEY = 'notejam-flask-secret-key'
-    #SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'notejam.db')
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'notejam.db')
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://' + os.environ.get("RDS_USERNAME") + ':' + os.environ.get("RDS_PASSWORD") + os.environ.get("RDS_ENDPOINT") + '/notejam'
 
 
