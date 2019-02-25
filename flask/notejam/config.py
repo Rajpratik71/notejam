ffcceef568cac70537362b11b3256d4c7f35b7ee
ramz <ramzthecoder@gmail.com>
2019-02-25 15:56:56 +1100

    Adding support for multiple environments

--- flask/notejam/config.py
+++ flask/notejam/config.py
@@ -7,17 +7,24 @@ class Config(object):
     SECRET_KEY = 'notejam-flask-secret-key'
     CSRF_ENABLED = True
     CSRF_SESSION_KEY = 'notejam-flask-secret-key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'notejam.db')
 
 
 class ProductionConfig(Config):
     DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
+class DevelopmentLocalConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'notejam.db')
 
 class DevelopmentConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 
 class TestingConfig(Config):
+    DEBUG = True
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
--- flask/runserver.py
+++ flask/runserver.py
@@ -1,7 +1,15 @@
+import os
 from notejam import app
-from notejam.config import DevelopmentConfig
+from notejam.config import DevelopmentConfig, TestingConfig, ProductionConfig, DevelopmentLocalConfig
 
-app.config.from_object(DevelopmentConfig)
+if os.getenv('ENV') == 'DEV':
+    app.config.from_object(DevelopmentConfig)
+elif os.getenv('ENV') == 'TST':
+    app.config.from_object(TestingConfig)
+elif os.getenv('ENV') == 'PRD':
+    app.config.from_object(ProductionConfig)
+else:
+    app.config.from_object(DevelopmentLocalConfig)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
